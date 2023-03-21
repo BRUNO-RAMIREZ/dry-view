7f88fa0d3fd27653c0ffb36665d8f56f9bd53f24
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-03-21 06:31:06 -0400

    Init project dry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ButtonModule } from 'ngx-dry';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponentesExampleComponent } from './view-componentes-example/view-componentes-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponentesExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-componentes-example/view-componentes-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-componentes-example',
+  templateUrl: './view-componentes-example.component.html',
+  styleUrls: ['./view-componentes-example.component.scss']
+})
+export class ViewComponentesExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
