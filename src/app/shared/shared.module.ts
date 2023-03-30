ad53a5a79d5e640d8f7e946277e6beb26a7166ff
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-03-29 22:11:46 -0400

    Creating component navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
