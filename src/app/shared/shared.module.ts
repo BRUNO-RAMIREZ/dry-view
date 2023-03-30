ffe99fd0c84c155ff14c60dc30c82a7f333b5c2d
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-03-29 23:27:43 -0400

    changes in sidebar and header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    NavbarComponent
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SidebarComponent,
-    NavbarComponent
+    HeaderComponent
   ]
 })
 export class SharedModule { }
