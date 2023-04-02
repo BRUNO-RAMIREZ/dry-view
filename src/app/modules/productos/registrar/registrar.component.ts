f1ff28cc150b5db25e4003c35fb2936c098c9903
Franz Gabriel <200908713@est.umss.edu>
2023-04-02 16:06:55 -0400

    creacion de productos

--- src/app/modules/productos/registrar/registrar.component.ts
+++ src/app/modules/productos/registrar/registrar.component.ts
@@ -1,4 +1,9 @@
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import { FormGroup,FormControl, FormBuilder, Validators } from '@angular/forms';
+  
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.component.scss']
 })
 export class RegistrarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public tituloForm;
+  
+  
+  get nombre(){
+    return this.formProduct.get('nombre')
   }
+  get descripcion(){
+    return this.formProduct.get('descripcion')
+  }
+  get precioCompra(){
+    return this.formProduct.get('precioCompra')
+  }
+  get precioVenta(){
+    return this.formProduct.get('precioVenta')
+  }
+  get cantidad(){
+    return this.formProduct.get('cantidad')
+  }
+
+   public formProduct:FormGroup;
+  constructor(private activatedroute:ActivatedRoute,
+              private fb: FormBuilder) { 
+  this.tituloForm = "";
+    this.formProduct=this.fb.group({
+      nombre:['',[Validators.required,Validators.minLength(4),Validators.maxLength(80)]],
+      descripcion:['',[Validators.required,Validators.minLength(4),Validators.maxLength(80)]],
+      precioCompra:['',Validators.required],
+      precioVenta:['',Validators.required],
+      cantidad:['',Validators.required],
+
+    });
+  }
+  public registrar(){
+
+  }
+  ngOnInit(): void {
+    this.activatedroute.params.subscribe(params =>{
+      if(params.id){
+       this.tituloForm = "Edicion de Productos";
+      }else{
+       this.tituloForm = "Registro de Productos";
+      }
+   });
+ }
 
 }
