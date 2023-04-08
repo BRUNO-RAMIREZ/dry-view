0ea1ebce08c6bec8f681390353968d01e1c69eff
Carlos Brian Revollo Villarroel <201900438@est.umss.edu>
2023-04-07 22:19:16 -0400

    Crud de producto

--- src/app/shared/ventana-confirmacion/ventana-confirmacion.component.ts
+++ src/app/shared/ventana-confirmacion/ventana-confirmacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'; 
+import {ProductModel} from '../../core/models/product.model';
 @Component({
   selector: 'app-ventana-confirmacion',
   templateUrl: './ventana-confirmacion.component.html',
@@ -8,8 +9,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class VentanaConfirmacionComponent implements OnInit {
 
   constructor(public dialogo: MatDialogRef<VentanaConfirmacionComponent>,
-    @Inject(MAT_DIALOG_DATA) public mensaje: string) { }
-
+    @Inject(MAT_DIALOG_DATA) public datos:{mensaje:string,pro:ProductModel}) {}
+   
   ngOnInit(): void {
   }
   public cerrarDialogo(): void {
