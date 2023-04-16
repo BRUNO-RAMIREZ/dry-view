b3efbf6c0fce6e8c0c466a8798ea70ea01380617
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-04-16 10:16:53 -0400

    Implementing router outlet of users and forms de register

--- src/app/modules/usuarios/usuarios-routing.module.ts
+++ src/app/modules/usuarios/usuarios-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeRouterOutletComponent} from "./pages/home-router-outlet/home-router-outlet.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {RegistrarComponent} from "./pages/registrar/registrar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeRouterOutletComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+     // {path: 'listado', component: ListadoComponent},
+      {path: 'registrar', component: RegistrarComponent},
+      {path: 'editar/:id', component: RegistrarComponent},
+      {path: '**', redirectTo: 'home'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SharedModule} from "../../shared/shared.module";
 
-import { UsuariosRoutingModule } from './usuarios-routing.module';
-
+import {UsuariosRoutingModule} from './usuarios-routing.module';
+import {RegistrarComponent} from './pages/registrar/registrar.component';
+import { HomeRouterOutletComponent } from './pages/home-router-outlet/home-router-outlet.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistrarComponent,
+    HomeRouterOutletComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
     UsuariosRoutingModule
   ]
 })
-export class UsuariosModule { }
+export class UsuariosModule {
+}
