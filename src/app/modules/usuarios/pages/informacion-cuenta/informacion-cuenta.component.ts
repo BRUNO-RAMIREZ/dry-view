08ba1f54af6448c56cc9c02a530ce37825b134b3
David Arcayne <arcayned6075@gmail.com>
2023-04-24 01:58:20 -0900

    no funciona el mostrar la informacion del usuario

--- src/app/modules/usuarios/pages/informacion-cuenta/informacion-cuenta.component.ts
+++ src/app/modules/usuarios/pages/informacion-cuenta/informacion-cuenta.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../../auth/services/auth.service';
+/*import { UsuariosService } from '../services/usuarios.service';*/
 @Component({
   selector: 'app-informacion-cuenta',
   templateUrl: './informacion-cuenta.component.html',
   styleUrls: ['./informacion-cuenta.component.scss']
 })
 export class InformacionCuentaComponent implements OnInit {
-
-  constructor() { }
+  public user: any;
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    //this.user = this.authService.getCredentials();
   }
 
 }
+    //this.user = this.authService.getCredentials();
+    //const credentials = this.authService.getCredentials();
+    //this.email = credentials.username;
+
+    //const credentials = this.authService.getCredentials();
+    //console.log(credentials);
