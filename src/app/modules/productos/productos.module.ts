0d507d75fcb4a160563c98c00fa7052b85cd928a
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-04-24 20:53:42 -0400

    Revert "changes"
    This reverts commit bc195fe9a69cacb812ea961ee2733b008857e3c0.

--- src/app/modules/productos/productos.module.ts
+++ src/app/modules/productos/productos.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from "@angular/router";
 import {NgxPaginationModule} from "ngx-pagination";
-import {ToastrService} from "ngx-toastr";
+import {ToastrModule, ToastrService} from "ngx-toastr";
 
 import {SharedModule} from "../../shared/shared.module";
 import {ProductosRoutingModule} from './productos-routing.module';
 import {ListadoComponent} from './pages/listado/listado.component';
+import {EditarComponent} from './pages/editar/editar.component';
 import {RegistrarComponent} from './pages/registrar/registrar.component';
 import {HomeRouterOutletComponent} from './pages/home-router-outlet/home-router-outlet.component';
 import {ProductosService} from "./services/productos.service";
@@ -17,6 +18,7 @@ import {SortByPipe} from './pipes/sort-by.pipe';
 import {HomeComponent} from "./pages/home/home.component";
 import {CardComponent} from './components/card/card.component';
 import {ChangeColorDirective} from './directives/change-color.directive';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 
 @NgModule({
@@ -24,6 +26,7 @@ import {ChangeColorDirective} from './directives/change-color.directive';
     HomeRouterOutletComponent,
     HomeComponent,
     ListadoComponent,
+    EditarComponent,
     RegistrarComponent,
     ModalComponent,
     FilterSearchPipeProducts,
