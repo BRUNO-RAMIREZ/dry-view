fd465ded2cc920b7a9d238cecc2ee4a8b4befceb
unknown <noeliafeliperamirez6@gmail.com>
2023-04-30 08:52:26 -0400

    fix error

--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -4,15 +4,16 @@ import {SharedModule} from "../../shared/shared.module";
 
 import {UsuariosRoutingModule} from './usuarios-routing.module';
 import {RegistrarComponent} from './pages/registrar/registrar.component';
-import {HomeRouterOutletComponent} from './pages/home-router-outlet/home-router-outlet.component';
-import {HomeComponent} from './pages/home/home.component';
+import { HomeRouterOutletComponent } from './pages/home-router-outlet/home-router-outlet.component';
+import { HomeComponent } from './pages/home/home.component';
 import {FilterSearchPipe} from "./pipes/filter-search.pipe";
 import {SortByPipe} from "./pipes/sort-by.pipe";
 import {NgxPaginationModule} from "ngx-pagination";
 import {CardComponent} from "./components/card/card.component";
 import {ProductosService} from "../productos/services/productos.service";
-import {ListadoComponent} from './pages/listado/listado.component';
-
+import {UsuariosService} from "./services/usuarios.service";
+import { ListadoComponent } from './pages/listado/listado.component';
+import { ModalComponent } from './componentes/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {ListadoComponent} from './pages/listado/listado.component';
     NgxPaginationModule
   ],
   providers: [
-    ProductosService
+    ProductosService,
+    UsuariosService
   ]
 })
 export class UsuariosModule {
