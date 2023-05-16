a827a6473cd84eafbee92d0602292049a700b6d3
Carlos Brian Revollo Villarroel <201900438@est.umss.edu>
2023-05-15 22:42:43 -0400

    Actualizacion interfaces ventas

--- src/app/core/models/ventas.model.ts
+++ src/app/core/models/ventas.model.ts
@@ -1,42 +1,40 @@
-
+  import {Cliente} from './client.model';
+  import {ProductGetByIdResponse} from './product.model';
   export interface VentasUpdateRequest {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
   
+
+
+
   export interface VentasUpdateResponse {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
   
   
   export interface VentasGetByIdResponse {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
   
   export interface VentasGetAllResponse {
@@ -45,14 +43,12 @@
   export interface VentasListResponse {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
   
 
@@ -60,25 +56,21 @@
   export interface VentasCreateRequest {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
   
   export interface VentasCreateResponse {
     id: number;
     code: string;
-    ci_Client: number;
-    id_seller:number;
-    lastName_Client: string;
-    ids_Products: number[];
-    amount_Product:number[];
+    client:Cliente;
+    products:ProductGetByIdResponse[];
+    total:number[];
     finalPrice: number;
-    dateTime: Date;
-    state:string;
+    dateSale: Date;
+    status:string;
   }
