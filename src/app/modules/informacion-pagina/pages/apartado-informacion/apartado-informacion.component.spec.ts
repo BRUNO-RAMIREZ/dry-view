7210a207d77f9461cae11ba87de4e27ba8128245
Carlos Brian Revollo Villarroel <201900438@est.umss.edu>
2023-05-25 01:52:45 -0400

    edicion Informacion completo

--- src/app/modules/informacion-pagina/pages/apartado-informacion/apartado-informacion.component.spec.ts
+++ src/app/modules/informacion-pagina/pages/apartado-informacion/apartado-informacion.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ApartadoInformacionComponent } from './apartado-informacion.component';
 
 describe('ApartadoInformacionComponent', () => {
